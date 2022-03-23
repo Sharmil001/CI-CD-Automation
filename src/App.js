9756c1185f5d6fb0a4c83b775188eb2c13f54d82
sharmil <sharmil.adroja@baygrape.in>
2022-03-23 11:55:44 +0530

    updated name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>HELLO FROM SHARMIL</p>
+        <p>HELLO FROM JENKINS</p>
         <a
           className="App-link"
           href="https://reactjs.org"
