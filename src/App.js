aa9807c0d034ce9954c4ff062001482119c9c4b8
sharmil <sharmil.adroja@baygrape.in>
2022-03-28 14:56:37 +0530

    heroku shell 1

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>ADDIN NEW CODE 001 HEROKU</p>
+        <p>ADDIN NEW CODE 001 HEROKU UPDATED</p>
         <a
           className="App-link"
           href="https://reactjs.org"
